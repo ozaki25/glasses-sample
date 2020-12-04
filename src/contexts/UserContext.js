1214e0b1c861f99ff814e469b7e5f0053ea8bbf6
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-12-04 19:46:56 +0900

    refactor

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,11 +1,6 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useState } from 'react';
 
 export const AuthUserContext = createContext(null);
-
-export function useAuthUser() {
-  const { authUser, setAuthUser } = useContext(AuthUserContext);
-  return { authUser, setAuthUser };
-}
 
 function AuthUserProvider(props) {
   const [authUser, setAuthUser] = useState(null);
