d29a7a1ec4965615bfaa44861191352d53f757f4
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-12-04 18:50:03 +0900

    add google login button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import { init } from './utils/firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
-init();
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_PROJECT_ID,
+} = process.env;
+
+const config = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: '/',
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+};
+
+firebase.initializeApp(config);
+
+console.log({ config });
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
     </div>
   );
 }
