02d0eab67c10f117b71c8ddf1df8ea145f6e09c7
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-12-04 18:35:48 +0900

    add firebase config

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import firebase from 'firebase/app';
+
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_PROJECT_ID,
+} = process.env;
+
+const config = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+firebase.initializeApp(config);
+
 function App() {
   return (
     <div>
